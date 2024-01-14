b909a82747fb30cbd6096360e0002133bc8309de
Josh Fettes <156533656+swankyjosh@users.noreply.github.com>
2024-01-13 22:28:47 -0600

    next

---
+++ java.js
@@ -0,0 +1,16 @@
+// Author: Josh Fettes
+// Date: 1/13/2024
+
+
+number = 5;
+
+
+newnumber = number * 2;
+
+
+food = prompt("What is your favorite food?");
+
+
+console.log(number);
+console.log(newnumber);
+console.log("Your favorite food is: " + food);
